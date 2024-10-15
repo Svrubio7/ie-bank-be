52edf5a4753437558aa968597c89b5c7a4befd51
Svrubio7 <svrubio7@gmail.com>
2024-10-15 11:10:50 +0200

    mod to innit

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 
-load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
+
 
 # Select environment based on the ENV environment variable
 if os.getenv('ENV') == 'local':
@@ -18,9 +18,6 @@ elif os.getenv('ENV') == 'dev':
 elif os.getenv('ENV') == 'ghci':
     print("Running in github mode")
     app.config.from_object('config.GithubCIConfig')
-else:
-    print("Running in production mode")
-    app.config.from_object('config.LocalConfig')
 
 db = SQLAlchemy(app)
 
